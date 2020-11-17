1288a2204c65ca19da02588fabe0f95d7f2bf9f0
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2020-11-17 13:22:49 +0530

    This program is to find the product of two numbers entered by the user

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+double a,b;
+double product=0.0;
+cout<<"Enter two numbers whose product has to be found out"<<endl;
+cin>>a>>b;
+product=a*b;
+cout<<"The product of"<<a<<"and"<<b<<"is"<<product<<endl;
+return 0;
+}
